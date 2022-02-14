e3050d5176baa424dde86416507cf4ca70c3f8f6
warlock <h.kwiatkowski@hotmail.com>
2022-02-14 15:58:57 +0100

    arrays

---
+++ phase3/section 1/zadanie3/main.js
@@ -0,0 +1,23 @@
+const italianUsers = ["Giovanni", "Michelle", "Angelo", "Luigi", "Valentina", "Marco"];
+console.log(italianUsers);
+
+// italianUsers.push("Alessandra");
+// console.log(italianUsers);
+
+// italianUsers.unshift("Tomasso");
+// console.log(italianUsers);
+
+// italianUsers.pop("Marco");
+// console.log(italianUsers);
+
+// delete italianUsers[italianUsers.length - 1];
+// console.log(italianUsers);
+
+// italianUsers.shift();
+// console.log(italianUsers);
+
+const redColors = ["czerwony", "zachodzącego słońca", "krwisty"];
+const greenColors = ["zielony", "trawiasty", "zielony banan"];
+
+const colors = redColors.concat(greenColors);
+console.log(colors);
