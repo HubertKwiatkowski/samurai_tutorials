dd45f1ab5035d924408cdcece50d6c2d583d86b8
warlock <h.kwiatkowski@hotmail.com>
2022-11-07 11:22:23 +0100

    ph04se03ex09

--- phase4/sec03/ex08/app.js
+++ phase4/sec03/ex08/app.js
@@ -1,5 +1,33 @@
-class Message extends ReactComponent {
+class Message extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      messageIsActive: false
+    }
+    this.handleMessageButton = this.handleMessageButton.bind(this)
+  }
+
+  handleMessageButton() {
+    this.setState({
+      messageIsActive: !this.state.messageIsActive
+    })
+  }
+
+  render() {
+    console.log(this.state.messageIsActive)
+    const text = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa cupiditate vel, nostrum perspiciatis repudiandae magnam obcaecati earum doloribus voluptatibus quod itaque, ea temporibus maxime non sint aspernatur eos? Magni, explicabo.'
+
+    return (
+      <>
+        <button onClick={this.handleMessageButton}>
+          {this.state.messageIsActive ? 'Ukryj' : 'Pokaz'}
+        </button>
+        {/* {this.state.messageIsActive ? <p>{text}</p> : null} */}
+        {this.state.messageIsActive && <p>{text}</p>}
+      </>
+    )
+  }
 }
 
 ReactDOM.render(<Message />, document.getElementById('root'))
