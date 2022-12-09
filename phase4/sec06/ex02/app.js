e53be75f1eeff7277d18b5574205e45567c25a01
warlock <h.kwiatkowski@hotmail.com>
2022-12-09 12:24:22 +0100

    ph04se06ex02

--- phase4/sec06/ex02/app.js
+++ phase4/sec06/ex02/app.js
@@ -1,4 +1,34 @@
+class App extends React.Component {
+  state = {
+    active: true
+  }
+
+  handleClick = () => {
+    this.setState(state => ({
+      active: !state.active
+    }))
+  }
+
+  render() {
+    return (
+      <div>
+        <SwitchButton active={this.state.active} click={this.handleClick}/>
+        <br />
+        <br />
+        {this.state.active && <Clock />}
+        
+      </div>
+    )
+  }
+}
+
+const SwitchButton = props => (
+  <button onClick={props.click}>{props.active ? "Wylacz" : "Wlacz"}</button>
+)
+
 class Clock extends React.Component {
+
+  interval = ""
 
   state = {
     time: this.getTime()
@@ -6,24 +36,20 @@ class Clock extends React.Component {
 
   getTime() {
     const currentTime = new Date();
-    // console.log(currentTime);
     return ({
       hours: currentTime.getHours(),
       minutes: currentTime.getMinutes(),
-      seconds: currentTime.getSeconds(),
-    }
-
-    )
+      seconds: currentTime.getSeconds()
+    })
   }
 
   setTime() {
-    // console.log(this);
     const time = this.getTime()
     this.setState({ time })
   }
 
   componentDidMount() {
-    this.interval = setInterval(() => this.setTime(), 1000)
+    this.interval = setInterval(this.setTime.bind(this), 1000)
   }
 
   componentWillUnmount() {
@@ -34,11 +60,10 @@ class Clock extends React.Component {
     const { hours, minutes, seconds } = this.state.time
     return (
       <div>
-        {hours} : {minutes} : {seconds}
+        {hours > 9 ? hours : `0${hours}`} : {minutes > 9 ? minutes : `0${minutes}`} : {seconds > 9 ? seconds : `0${seconds}`}
       </div>
     )
   }
 }
 
-
-ReactDOM.render(<Clock />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
